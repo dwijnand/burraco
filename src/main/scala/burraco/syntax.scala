b9c3d92976ffab9e050e9043aa56a0a1b0637629
Dale Wijnand <dale.wijnand@gmail.com>
2016-05-06 13:29:13 +0100

    Consolidate macros & move to whitebox

--- src/main/scala/burraco/syntax.scala
+++ src/main/scala/burraco/syntax.scala
@@ -1,30 +1,44 @@
 package burraco
 
-import scala.reflect.macros.blackbox
+import scala.reflect.macros.whitebox
 
 final class IntToRank(private val i: Int) extends AnyVal {
-  def rank: Rank = macro IntToRankMacros.rankImpl
+  def rank: Rank = macro CardMacros.intToRankImpl
 }
 
 final class RankToCard(private val rank: Rank) extends AnyVal {
-  def    clubs: RegularCard = macro RankToCardMacros.rankToCardImpl
-  def diamonds: RegularCard = macro RankToCardMacros.rankToCardImpl
-  def   hearts: RegularCard = macro RankToCardMacros.rankToCardImpl
-  def   spades: RegularCard = macro RankToCardMacros.rankToCardImpl
+  def    clubs: RegularCard = macro CardMacros.rankToCardImpl
+  def diamonds: RegularCard = macro CardMacros.rankToCardImpl
+  def   hearts: RegularCard = macro CardMacros.rankToCardImpl
+  def   spades: RegularCard = macro CardMacros.rankToCardImpl
 }
 
 final class IntToCard(private val i: Int) extends AnyVal {
-  def    clubs: RegularCard = macro IntToCardMacros.intToCardImpl
-  def diamonds: RegularCard = macro IntToCardMacros.intToCardImpl
-  def   hearts: RegularCard = macro IntToCardMacros.intToCardImpl
-  def   spades: RegularCard = macro IntToCardMacros.intToCardImpl
+  def    clubs: RegularCard = macro CardMacros.intToCardImpl
+  def diamonds: RegularCard = macro CardMacros.intToCardImpl
+  def   hearts: RegularCard = macro CardMacros.intToCardImpl
+  def   spades: RegularCard = macro CardMacros.intToCardImpl
 }
 
-trait IntToRankMacroFn {
-  val c: blackbox.Context
+final class CardMacros(val c: whitebox.Context) {
   import c.universe._
 
-  def intToRank(x: Any): Tree = x match {
+  def intToRankImpl: Tree = c.prefix.tree match {
+    case Apply(_, Seq(Literal(Constant(x)))) => intToRank(x)
+  }
+
+  def rankToCardImpl: Tree = c.macroApplication match {
+    case Select(Apply(_, Seq(rank)), TermName(suitStr)) => createCard(rank, suitStr)
+  }
+
+  def intToCardImpl: Tree = c.macroApplication match {
+    case Select(Apply(_, Seq(x)), suit: TermName) => q"$x.rank.$suit"
+  }
+
+  private def createCard(rank: Tree, suitStr: String): Tree =
+    q"_root_.burraco.RegularCard($rank, ${stringToSuit(suitStr)})"
+
+  private def intToRank(x: Any): Tree = x match {
     case 1  => q"_root_.burraco.Ace"
     case 2  => q"_root_.burraco.Deuce"
     case 3  => q"_root_.burraco.Rank._3"
@@ -39,42 +53,11 @@ trait IntToRankMacroFn {
     case 12 => q"_root_.burraco.Queen"
     case 13 => q"_root_.burraco.King"
   }
-}
 
-trait StringToSuitMacroFn {
-  val c: blackbox.Context
-  import c.universe._
-
-  def stringToSuit(s: String): Tree = s match {
+  private def stringToSuit(s: String): Tree = s match {
     case "clubs"    => q"_root_.burraco.Clubs"
     case "diamonds" => q"_root_.burraco.Diamonds"
     case "hearts"   => q"_root_.burraco.Hearts"
     case "spades"   => q"_root_.burraco.Spades"
-  }
-}
-
-final class IntToRankMacros(val c: blackbox.Context) extends IntToRankMacroFn {
-  import c.universe._
-
-  def rankImpl: Tree = c.prefix.tree match {
-    case Apply(_, Seq(Literal(Constant(x)))) => intToRank(x)
-  }
-}
-
-final class RankToCardMacros(val c: blackbox.Context) extends StringToSuitMacroFn {
-  import c.universe._
-
-  def rankToCardImpl: Tree = c.macroApplication match {
-    case Select(Apply(_, Seq(rank)), TermName(suitStr)) =>
-      q"_root_.burraco.RegularCard($rank, ${stringToSuit(suitStr)})"
-  }
-}
-
-final class IntToCardMacros(val c: blackbox.Context) extends IntToRankMacroFn with StringToSuitMacroFn {
-  import c.universe._
-
-  def intToCardImpl: Tree = c.macroApplication match {
-    case Select(Apply(_, Seq(Literal(Constant(x)))), TermName(suitStr)) =>
-      q"_root_.burraco.RegularCard(${intToRank(x)}, ${stringToSuit(suitStr)})"
   }
 }
